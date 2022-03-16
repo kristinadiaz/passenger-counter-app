319ca8e1ee458ba6e43ca67a4b7e17005f79fc10
Kristina Diaz <kristina.diaz84@gmail.com>
2022-03-15 19:07:07 -0600

    started adding functionality to button

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-let count = 5
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
-console.log(count)
+function increment() {
+    console.log("The button was clicked")
+}
