01367c3d7ce993c747cc86c8fc0ddd86da37c761
Kristina Diaz <kristina.diaz84@gmail.com>
2022-03-16 14:15:54 -0600

    made increment button active

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
+// increment count when button is clicked
+
+let countEl = document.getElementById("count-el");
+let count = 0;
 
 function increment() {
-    console.log("The button was clicked")
-}
+    count += 1;
+    countEl.innerText = count;
+};
